7bc76191b72a3051528acde4d420bfccc9d81e08
Sagarika0306 <sagarikaroychowdhury454@gmail.com>
2024-11-28 00:19:27 +0530

    Add files via upload

---
+++ starAii.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    i=1;
+    while(i<=3)
+    {
+        printf("\n");
+        j=1;
+        while(j<=i)
+        {
+            printf("%d",i);
+            j++;
+        }
+        i++;
+    }
+}
